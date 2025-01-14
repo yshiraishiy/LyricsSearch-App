caf79f82e873d8254f20913bc4eb2e7bdedaf302
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-14 13:53:06 +0900

    ✨曲の歌詞を取得・表示

--- src/main.js
+++ src/main.js
@@ -78,6 +78,21 @@ async function getMoreSongs(url) {
   showData(data);
 }
 
+// 歌から歌詞を取得
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+
+  result.innerHTML = `
+  <h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>
+  `;
+
+  more.innerHTML = "";
+}
+
 // イベントリスナー
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -88,5 +103,17 @@ form.addEventListener("submit", (e) => {
     alert("Please type in a search term");
   } else {
     searchSongs(searchTerm);
+  }
+});
+
+// 歌詞を取得
+result.addEventListener("click", (e) => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === "BUTTON") {
+    const artist = clickedEl.getAttribute("data-artist");
+    const songTitle = clickedEl.getAttribute("data-songtitle");
+
+    getLyrics(artist, songTitle);
   }
 });
